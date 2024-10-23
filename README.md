51c8f33af21be94726c54eadf8f30be6c589b2d8
ahambly23 <ashleyhambly4@hotmail.com>
2024-10-23 12:42:07 +0100

    Completed challenge. Added further challenges to the README

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Context: You’re part of a research team that has found a new mysterious organism at the bottom of the ocean near hydrothermal vents. Your team names the organism, Pila aequor (P. aequor), and finds that it is only comprised of 15 DNA bases. The small DNA samples and frequency at which it mutates due to the hydrothermal vents make P. aequor an interesting specimen to study. However, P. aequor cannot survive above sea level and locating P. aequor in the deep sea is difficult and expensive. Your job is to create objects that simulate the DNA of P. aequor for your research team to study.
 
 As you progress through the steps, use the terminal and console.log() statements to check the output of your loops and functions.
+
+
+If you’d like to challenge yourself further, you could consider the following:
+
+Create a .complementStrand() method to the factory function’s object that returns the complementary DNA strand. The rules are that 'A's match with 'T's and vice versa. Also, 'C's match with 'G's and vice versa. (Check the hint for more details)
+
+Use the .compareDNA() to find the two most related instances of pAequor.
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -13,10 +13,59 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// factory function pAequorFactory()
+const pAequorFactory = (number, array) => {
+  let object = {
+    specimenNum: number,
+    dna: array,
+    mutate() {
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      const currentBase = this.dna[randomIndex];
+      const dnaBases = ["A", "T", "C", "G"];
+      const possibleBases = dnaBases.filter((base) => base !== currentBase);
+      const newBase =
+        possibleBases[Math.floor(Math.random() * possibleBases.length)];
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    },
+    compareDNA(obj) {
+      let identicalBases = 0;
+      let totalBases = this.dna.length;
+      for (let x = 0; x < totalBases; x++) {
+        if (this.dna[x] === obj.dna[x]) {
+          identicalBases++;
+        }
+      }
+      let percentage = 0;
+      if (totalBases !== 0) {
+        percentage = (identicalBases / totalBases) * 100;
+        console.log(
+          `Specimen #${this.specimenNum} and Specimen #${obj.specimenNum} have ${percentage}% in common.`
+        );
+      }
+    },
+    willLikelySurvive() {
+      let goodDNA = this.dna.filter((base) => {
+        return base === "C" || base === "G";
+      });
+      if ((goodDNA.length / this.dna.length) * 100 >= 60) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  };
+  return object;
+};
 
+const instances = 30;
+let survivingPAequor = [];
 
+while (survivingPAequor.length < instances) {
+  let newInstance = pAequorFactory(survivingPAequor.length + 1, mockUpStrand());
+  if (newInstance.willLikelySurvive()) {
+    survivingPAequor.push(newInstance);
+  }
+}
 
-
-
-
-
+console.log(survivingPAequor)
